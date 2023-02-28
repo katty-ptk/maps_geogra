3811686d178f0482c4b60a0b361d54eaef6d6b3f
Katty Ptk <46168818+katty-ptk@users.noreply.github.com>
2023-02-28 20:35:20 +0200

    added marker to home (#2)
    * added marker to home
    
    * continued work on markers:
    	-- made array of locations ( objects with location name, latitude, and longiture
    	-- on initState, add every marker from that array to the list ( before, every marker was added manually
    	-- on marker tap, show nice scrollable bottom modal
    
    * wrote function for creating marker to not do that in initState
    
    ---------
    
    Co-authored-by: Katty Ptk <kattyptk@Kattys-MacBook-Pro.local>

--- lib/screens/app/HomePage.dart
+++ lib/screens/app/HomePage.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: file_names
-
 import 'dart:async';
 
 import 'package:flutter/material.dart';
@@ -13,13 +12,84 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late GoogleMapController mapController; 
+
+  // ignore: prefer_collection_literals
+  Set<Marker> markers = Set();
+
+  final customMarkers = [
+    {
+      "title": "Timisoara", 
+      "lat": 45.7411191,
+      "lng": 21.1815705
+    },
+
+    {
+      "title": "Bucuresti", 
+      "lat": 44.4379269,
+      "lng": 26.0245983
+    }
+  ];
+
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
 
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(46.4298523, 21.8240082),
     zoom: 10,
-  );
+  );  
+
+  @override
+  void initState() {
+    addMarkers();
+    super.initState();
+  }
+
+  addMarkers() {
+    for (var marker in customMarkers) {
+      markers.add(Marker(
+        markerId: MarkerId("_${marker["title"]}kGooglrrePlex"),
+        infoWindow: InfoWindow(
+          title: marker["title"].toString(),
+          snippet: "this is a snippet"
+        ),
+        // icon: BitmapDescriptor.fromBytes(customMarker, size: const Size(3, 3)),
+        icon: BitmapDescriptor.defaultMarker,
+        position: LatLng(marker["lat"] as double, marker["lng"] as double),
+        onTap: () {
+          showModalBottomSheet(
+            context: context, 
+            isScrollControlled: true,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(30)
+              )
+            ),
+            builder: (BuildContext context ) {
+              return DraggableScrollableSheet(
+                expand: false,
+                initialChildSize: 0.4,
+                minChildSize: 0.32,
+                maxChildSize: 0.9,
+                builder:(context, scrollController) =>
+                 SingleChildScrollView(
+                  controller: scrollController,
+                  child: Center(
+                    child: ElevatedButton(
+                      child: const Text("close"),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ),
+                ),
+              );
+            }
+          );
+        }
+      ));
+    }
+  }
 
   static const CameraPosition _kLake = CameraPosition(
       bearing: 192.8334901395799,
@@ -34,15 +104,12 @@ class _HomePageState extends State<HomePage> {
       body: GoogleMap(
         mapType: MapType.normal,
         initialCameraPosition: _kGooglePlex,
+        markers: markers,
         onMapCreated: (GoogleMapController controller) {
+          mapController = controller;
           _controller.complete(controller);
         },
       ),
-      // floatingActionButton: FloatingActionButton.extended(
-      //   onPressed: _goToTheLake,
-      //   label: const Text('To the lake!'),
-      //   icon: const Icon(Icons.directions_boat),
-      // ),
     );
   }
 
