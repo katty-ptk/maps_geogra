103879160161498b8452dc875f7caff4edd2a6d4
Katty Ptk <kattyptk@kattys-mbp.home>
2023-04-10 11:03:25 +0300

    changed logo image

--- lib/widgets/place_info.dart
+++ lib/widgets/place_info.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 
 class PlaceInfo extends StatelessWidget {
   late BuildContext context;
-  late ScrollController scrollController;
+  late ScrollController scrollController; 
   // ignore: non_constant_identifier_names, prefer_typing_uninitialized_variables
   late var placeImages;
 
